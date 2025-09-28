75e501d64af68a37b993de8a548b810036fa324d
DmitryLoginov <daimom25@gmail.com>
2025-09-28 16:08:40 +0300

    feat(ExceptionConstants): добавить класс для констант ошибок

---
+++ src/main/java/ru/practicum/shareit/exception/ExceptionConstants.java
@@ -0,0 +1,9 @@
+package ru.practicum.shareit.exception;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ExceptionConstants {
+    public static final String USER_NOT_FOUND_BY_ID = "Пользователь с id = %d не найден";
+}
