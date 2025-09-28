53ad67036eedbe363deeb282b10713e0aa056313
DmitryLoginov <daimom25@gmail.com>
2025-09-28 14:25:59 +0300

    feat(UserMapper): создать класс маппер

---
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package ru.practicum.shareit.user.mapper;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class UserMapper {
+    public static UserDto toUserDto(User user) {
+        UserDto userDto = new UserDto();
+
+        userDto.setId(user.getId());
+        userDto.setName(user.getName());
+        userDto.setEmail(user.getEmail());
+
+        return userDto;
+    }
+}
