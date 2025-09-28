d8bc60304de9683bde8360b4372ad58856dcc114
DmitryLoginov <daimom25@gmail.com>
2025-09-28 14:23:32 +0300

    feat(ItemMapper): создать класс маппер

---
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -0,0 +1,22 @@
+package ru.practicum.shareit.item.mapper;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ItemMapper {
+    public static ItemDto toItemDto(Item item) {
+        ItemDto itemDto = new ItemDto();
+
+        itemDto.setId(item.getId());
+        itemDto.setOwnerId(item.getOwner().getId());
+        itemDto.setName(item.getName());
+        itemDto.setDescription(item.getDescription());
+        itemDto.setAvailable(item.isAvailable());
+        itemDto.setRequestId(item.getRequest().getId());
+
+        return itemDto;
+    }
+}
