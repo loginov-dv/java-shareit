7883d389e493d946b6d598312ff1e3383a12113b
DmitryLoginov <daimom25@gmail.com>
2025-09-28 14:30:24 +0300

    feat(ItemRequestDto): добавить поля в класс ItemRequestDto

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,7 +1,13 @@
 package ru.practicum.shareit.request.dto;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class ItemRequestDto {
+    private Integer id;
+    private String description;
+    private Integer requestorId;
+    private LocalDateTime created;
 }
