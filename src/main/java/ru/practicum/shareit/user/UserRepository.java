cb10b860b9f4f1a2eda01c73a4c7aba3fe0d3c83
DmitryLoginov <daimom25@gmail.com>
2025-09-28 16:09:15 +0300

    feat(UserRepository): добавить интерфейс-репозиторий пользователей

---
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -0,0 +1,15 @@
+package ru.practicum.shareit.user;
+
+import ru.practicum.shareit.user.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository {
+    User save(User user);
+
+    Optional<User> findById(int userId);
+
+    void update(User user);
+
+    void delete(int userId);
+}
