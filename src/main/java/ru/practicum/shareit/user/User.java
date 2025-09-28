7df7655b8613e2a4166543da4a66291fe94bc910
DmitryLoginov <daimom25@gmail.com>
2025-09-28 14:06:49 +0300

    feat(User): добавить поля в класс User

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,12 @@
 package ru.practicum.shareit.user;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(of = "email")
 public class User {
+    private Integer id;
+    private String name;
+    private String email;
 }
