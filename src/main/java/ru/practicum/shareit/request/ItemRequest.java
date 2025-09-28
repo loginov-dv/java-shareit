036f92574552053c9588cc912e1d32f0602053c5
DmitryLoginov <daimom25@gmail.com>
2025-09-28 14:06:38 +0300

    feat(ItemRequest): добавить поля в класс ItemRequest

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,7 +1,15 @@
 package ru.practicum.shareit.request;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode(of = "id")
 public class ItemRequest {
+    private Integer id;
+    private String description;
+    private Integer requestorId;
+    private LocalDateTime created;
 }
