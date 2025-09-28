17b2e323c9ec652aff86d0f40a25de84da018750
DmitryLoginov <daimom25@gmail.com>
2025-09-28 14:22:51 +0300

    fix(Item): скорректировать наименования и типы полей

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -2,14 +2,16 @@ package ru.practicum.shareit.item.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.model.User;
 
 @Data
 @EqualsAndHashCode(of = "id")
 public class Item {
     private Integer id;
-    private Integer ownerId;
+    private User owner;
     private String name;
     private String description;
-    private boolean availableForRent;
-    private Integer requestId;
+    private boolean available;
+    private ItemRequest request;
 }
