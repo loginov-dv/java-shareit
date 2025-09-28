0ebf82553ed88b43d7d7bfb6c006ed408d65deb4
DmitryLoginov <daimom25@gmail.com>
2025-09-28 14:07:39 +0300

    feat(Booking): добавить поля в класс Booking

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,17 @@
 package ru.practicum.shareit.booking;
 
-/**
- * TODO Sprint add-bookings.
- */
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode(of = "id")
 public class Booking {
+    private Integer id;
+    private Integer itemId;
+    private Integer bookerId;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private BookingStatus status;
 }
