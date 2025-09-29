fe5d50619bb0c0914ba0dd206c4f66a8b0f285ca
DmitryLoginov <daimom25@gmail.com>
2025-09-29 21:14:42 +0300

    feat(ExceptionConstants): добавить константу

--- src/main/java/ru/practicum/shareit/exception/ExceptionConstants.java
+++ src/main/java/ru/practicum/shareit/exception/ExceptionConstants.java
@@ -7,4 +7,6 @@ import lombok.NoArgsConstructor;
 public final class ExceptionConstants {
     public static final String USER_NOT_FOUND_BY_ID = "Пользователь с id = %d не найден";
     public static final String EMAIL_CONFLICT = "Этот email уже используется";
+    public static final String ITEM_NOT_FOUND_BY_ID = "Предмет с id = %d не найден";
+    public static final String NO_ACCESS_FOR_EDIT = "Редактировать вещь может только её владелец";
 }
