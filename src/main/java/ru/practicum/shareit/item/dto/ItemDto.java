db0c9b89897f5ed3610f3b1c2f376db93dff82d5
DmitryLoginov <daimom25@gmail.com>
2025-09-29 21:10:21 +0300

    feat(ItemDto): скорректировать типы полей, добавить аннотации валидации

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,13 +1,18 @@
 package ru.practicum.shareit.item.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class ItemDto {
     private Integer id;
     private Integer ownerId;
+    @NotBlank(message = "Наименование не может быть пустым")
     private String name;
+    @NotBlank(message = "Описание не может быть пустым")
     private String description;
-    private boolean available;
+    @NotNull(message = "Статус должен быть задан")
+    private Boolean available;
     private Integer requestId;
 }
