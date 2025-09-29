e983bf7cc35c40f88fed708cee545050dace6705
DmitryLoginov <daimom25@gmail.com>
2025-09-29 21:09:57 +0300

    feat(Item): скорректировать наименования и типы полей

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -2,16 +2,14 @@ package ru.practicum.shareit.item.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import ru.practicum.shareit.request.model.ItemRequest;
-import ru.practicum.shareit.user.model.User;
 
 @Data
 @EqualsAndHashCode(of = "id")
 public class Item {
     private Integer id;
-    private User owner;
+    private Integer ownerId;
     private String name;
     private String description;
     private boolean available;
-    private ItemRequest request;
+    private Integer requestId;
 }
