302e4b562542f6195fc0d568a29173e0e5f79126
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-10-13 22:27:14 +0300

    feat(InMemoryUserRepository): убрать связь с UserRepository

--- src/main/java/ru/practicum/shareit/user/InMemoryUserRepository.java
+++ src/main/java/ru/practicum/shareit/user/InMemoryUserRepository.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.user;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.user.model.User;
 
 import java.util.HashMap;
@@ -10,12 +9,10 @@ import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
-@Repository
-public class InMemoryUserRepository implements UserRepository {
+public class InMemoryUserRepository {
     private final Map<Integer, User> users = new HashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
-    @Override
     public User save(User user) {
         log.debug("Запрос на создание пользователя");
         user.setId(counter.incrementAndGet());
@@ -24,13 +21,11 @@ public class InMemoryUserRepository implements UserRepository {
         return user;
     }
 
-    @Override
     public Optional<User> findById(int userId) {
         log.debug("Запрос на получение пользователя с id = {}", userId);
         return Optional.ofNullable(users.get(userId));
     }
 
-    @Override
     public Optional<User> findByEmail(String email) {
         log.debug("Запрос на получение пользователя с email = {}", email);
         return users.values().stream()
@@ -38,13 +33,11 @@ public class InMemoryUserRepository implements UserRepository {
                 .findFirst();
     }
 
-    @Override
     public void update(User user) {
         log.debug("Запрос на изменение пользователя с id = {}", user.getId());
         users.put(user.getId(), user);
     }
 
-    @Override
     public void deleteById(int userId) {
         log.debug("Запрос на удаление пользователя с id = {}", userId);
         users.remove(userId);
