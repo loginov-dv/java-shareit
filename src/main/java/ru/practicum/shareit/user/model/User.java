15d5644c45d7ec47d815c4c8b15ca0cfc9ecfa84
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-10-13 22:26:09 +0300

    feat(User): добавить аннотации JPA

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,12 +1,20 @@
 package ru.practicum.shareit.user.model;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Data
-@EqualsAndHashCode(of = "email")
+@Entity
+@Table(name = "users")
+@Getter @Setter @ToString
+@EqualsAndHashCode(of = {"email"})
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "email")
     private String email;
 }
