749a59e338f6348c4039d7ceb2c68b264568f8a7
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-10-16 21:29:57 +0300

    feat: добавить константы логирования и исключений

--- src/main/java/ru/practicum/shareit/exception/ExceptionConstants.java
+++ src/main/java/ru/practicum/shareit/exception/ExceptionConstants.java
@@ -7,6 +7,21 @@ import lombok.NoArgsConstructor;
 public final class ExceptionConstants {
     public static final String USER_NOT_FOUND_BY_ID = "Пользователь с id = %d не найден";
     public static final String EMAIL_CONFLICT = "Этот email уже используется";
+
     public static final String ITEM_NOT_FOUND_BY_ID = "Предмет с id = %d не найден";
-    public static final String NO_ACCESS_FOR_EDIT = "Редактировать вещь может только её владелец";
+
+    public static final String NO_ACCESS = "Нет доступа";
+
+    public static final String INCORRECT_BOOKING_DATES_ORDER = "Дата окончания бронирования должна быть " +
+            "после даты начала бронирования";
+    public static final String START_DATE_EQUALS_TO_END_DATE = "Дата окончания бронирования не может " +
+            "совпадать с датой начала бронирования";
+    public static final String START_DATE_IN_THE_PAST = "Дата начала бронирования не может быть в прошлом";
+    public static final String END_DATE_IN_THE_PAST = "Дата окончания бронирования не может быть в прошлом";
+
+    public static final String BOOKING_NOT_FOUND_BY_ID = "Бронирование с id = %d не найдено";
+
+    public static final String INVALID_BOOKING_STATE = "Некорректное значение статуса для запроса бронирований";
+
+    public static final String HAS_NO_COMPLETED_BOOKINGS = "Невозможно оставить комментарий (нет завершённой аренды)";
 }
--- src/main/java/ru/practicum/shareit/exception/LogConstants.java
+++ src/main/java/ru/practicum/shareit/exception/LogConstants.java
@@ -7,5 +7,17 @@ import lombok.NoArgsConstructor;
 public final class LogConstants {
     public static final String USER_NOT_FOUND_BY_ID = "Пользователь с id = {} не найден";
     public static final String EMAIL_CONFLICT = "email = {} уже используется";
+
     public static final String ITEM_NOT_FOUND_BY_ID = "Предмет с id = {} не найден";
+
+    public static final String INCORRECT_BOOKING_DATES_ORDER = "Дата окончания бронирования должна быть " +
+            "после даты начала бронирования";
+    public static final String START_DATE_EQUALS_TO_END_DATE = "Дата окончания бронирования не может " +
+            "совпадать с датой начала бронирования";
+    public static final String START_DATE_IN_THE_PAST = "Дата начала бронирования не может быть в прошлом";
+    public static final String END_DATE_IN_THE_PAST = "Дата окончания бронирования не может быть в прошлом";
+
+    public static final String BOOKING_NOT_FOUND_BY_ID = "Бронирование с id = {} не найдено";
+
+    public static final String INVALID_BOOKING_STATE = "Некорректное значение статуса для запроса бронирований";
 }
