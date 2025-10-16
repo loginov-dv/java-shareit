4d96d58a952cf5527ae2b9f9a565ac4cd0cf8c45
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-10-16 21:25:45 +0300

    fix(BookingDto): скорректировать типы полей

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,15 +1,15 @@
 package ru.practicum.shareit.booking.dto;
 
 import lombok.Data;
-
-import java.time.LocalDateTime;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.user.dto.UserDto;
 
 @Data
 public class BookingDto {
     private Integer id;
-    private Integer itemId;
-    private Integer bookerId;
-    private LocalDateTime start;
-    private LocalDateTime end;
+    private ItemDto item;
+    private UserDto booker;
+    private String start;
+    private String end;
     private String status;
 }
