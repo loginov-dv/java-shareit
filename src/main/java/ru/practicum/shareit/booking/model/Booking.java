1dc86b646495addd7184713fba6d10721fafbb41
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-10-16 21:23:31 +0300

    feat(Booking): добавить аннотации JPA

--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -1,19 +1,38 @@
 package ru.practicum.shareit.booking.model;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import jakarta.persistence.*;
+import lombok.*;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
-@Data
-@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "bookings")
+@Getter @Setter @ToString
+@EqualsAndHashCode(of = {"id"})
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    @ToString.Exclude
     private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "booker_id")
+    @ToString.Exclude
     private User booker;
-    private LocalDateTime start;
-    private LocalDateTime end;
-    private BookingStatus status;
+
+    @Column(name = "start_date")
+    private Instant start;
+
+    @Column(name = "end_date")
+    private Instant end;
+
+    @Column(name = "status")
+    @Enumerated(EnumType.STRING)
+    private BookingStatus status = BookingStatus.WAITING;
 }
