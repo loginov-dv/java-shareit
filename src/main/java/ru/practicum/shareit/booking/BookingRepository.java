02d7362d8c1fc795231d6efd128c8bd3efc9557a
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-10-16 22:31:45 +0300

    fix(BookingRepository): заменить функции SQL, возвращающие текущую метку времени, на внешний передаваемый параметр метода

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.booking.model.Booking;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
@@ -14,38 +15,38 @@ public interface BookingRepository extends JpaRepository<Booking, Integer> {
     List<Booking> findByBookerIdOrderByStartDesc(int bookerId);
 
     @Query("SELECT booking FROM Booking booking WHERE " +
-            "booking.booker.id = ?1 AND booking.end < LOCALTIMESTAMP " +
+            "booking.booker.id = ?1 AND booking.end < ?2 " +
             "ORDER BY booking.start DESC")
-    List<Booking> findPastByBookerId(int bookerId);
+    List<Booking> findPastByBookerIdAndEndIsBefore(int bookerId, LocalDateTime end);
 
     @Query("SELECT booking FROM Booking booking WHERE " +
-            "booking.booker.id = ?1 AND booking.start > LOCALTIMESTAMP " +
+            "booking.booker.id = ?1 AND booking.start > ?2 " +
             "ORDER BY booking.start DESC")
-    List<Booking> findFutureByBookerId(int bookerId);
+    List<Booking> findFutureByBookerIdAndStartIsAfter(int bookerId, LocalDateTime start);
 
     @Query("SELECT booking FROM Booking booking WHERE " +
-            "booking.booker.id = ?1 AND (LOCALTIMESTAMP BETWEEN booking.start AND booking.end) " +
+            "booking.booker.id = ?1 AND (?2 BETWEEN booking.start AND booking.end) " +
             "ORDER BY booking.start DESC")
-    List<Booking> findCurrentByBookedId(int bookerId);
+    List<Booking> findCurrentByBookedId(int bookerId, LocalDateTime now);
 
     List<Booking> findByItemOwnerIdAndStatusOrderByStartDesc(int ownerId, String status);
 
     List<Booking> findByItemOwnerIdOrderByStartDesc(int ownerId);
 
     @Query("SELECT booking FROM Booking booking WHERE " +
-            "booking.item.owner.id = ?1 AND booking.end < LOCALTIMESTAMP " +
+            "booking.item.owner.id = ?1 AND booking.end < ?2 " +
             "ORDER BY booking.start DESC")
-    List<Booking> findPastByOwnerId(int ownerId);
+    List<Booking> findPastByOwnerIdAndEndIsBefore(int ownerId, LocalDateTime end);
 
     @Query("SELECT booking FROM Booking booking WHERE " +
-            "booking.item.owner.id = ?1 AND booking.start > LOCALTIMESTAMP " +
+            "booking.item.owner.id = ?1 AND booking.start > ?2 " +
             "ORDER BY booking.start DESC")
-    List<Booking> findFutureByOwnerId(int ownerId);
+    List<Booking> findFutureByOwnerIdAndStartIsAfter(int ownerId, LocalDateTime start);
 
     @Query("SELECT booking FROM Booking booking WHERE " +
-            "booking.item.owner.id = ?1 AND (LOCALTIMESTAMP BETWEEN booking.start AND booking.end) " +
+            "booking.item.owner.id = ?1 AND (?2 BETWEEN booking.start AND booking.end) " +
             "ORDER BY booking.start DESC")
-    List<Booking> findCurrentByOwnerId(int ownerId);
+    List<Booking> findCurrentByOwnerId(int ownerId, LocalDateTime now);
 
     List<Booking> findByItemIdOrderByStartDesc(int itemId);
 
