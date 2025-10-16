b93d8f287609041c9aad1c6db05e7cd223f54c17
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-10-16 21:30:39 +0300

    feat(Comment): добавить модель данных для комментария

---
+++ src/main/java/ru/practicum/shareit/item/model/Comment.java
@@ -0,0 +1,36 @@
+package ru.practicum.shareit.item.model;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "comments")
+@Getter @Setter @ToString
+@EqualsAndHashCode(of = {"id"})
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "text")
+    private String text;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    @ToString.Exclude
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    @ToString.Exclude
+    private User author;
+
+    @Column(name = "date_created")
+    private Instant created = Instant.now();
+}
