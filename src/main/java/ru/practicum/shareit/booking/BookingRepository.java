4af0e08e8a9134c9db70262403cb21f6641c405c
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-10-17 18:15:30 +0300

    refactor(BookingRepository): замена JPQL-запросов на запросные методы

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -14,34 +14,26 @@ public interface BookingRepository extends JpaRepository<Booking, Integer> {
 
     List<Booking> findByBookerIdOrderByStartDesc(int bookerId);
 
-    @Query("SELECT booking FROM Booking booking WHERE " +
-            "booking.booker.id = ?1 AND booking.end < ?2 " +
-            "ORDER BY booking.start DESC")
-    List<Booking> findPastByBookerIdAndEndIsBefore(int bookerId, LocalDateTime end);
+    // Запрос завершённых бронирований
+    List<Booking> findByBookerIdAndEndBeforeOrderByStartDesc(int bookerId, LocalDateTime end);
 
-    @Query("SELECT booking FROM Booking booking WHERE " +
-            "booking.booker.id = ?1 AND booking.start > ?2 " +
-            "ORDER BY booking.start DESC")
-    List<Booking> findFutureByBookerIdAndStartIsAfter(int bookerId, LocalDateTime start);
+    // Запрос предстоящих бронирований
+    List<Booking> findByBookerIdAndStartAfterOrderByStartDesc(int bookerId, LocalDateTime start);
 
     @Query("SELECT booking FROM Booking booking WHERE " +
             "booking.booker.id = ?1 AND (?2 BETWEEN booking.start AND booking.end) " +
             "ORDER BY booking.start DESC")
-    List<Booking> findCurrentByBookedId(int bookerId, LocalDateTime now);
+    List<Booking> findCurrentByBookerId(int bookerId, LocalDateTime now);
 
     List<Booking> findByItemOwnerIdAndStatusOrderByStartDesc(int ownerId, String status);
 
     List<Booking> findByItemOwnerIdOrderByStartDesc(int ownerId);
 
-    @Query("SELECT booking FROM Booking booking WHERE " +
-            "booking.item.owner.id = ?1 AND booking.end < ?2 " +
-            "ORDER BY booking.start DESC")
-    List<Booking> findPastByOwnerIdAndEndIsBefore(int ownerId, LocalDateTime end);
+    // Запрос завершённых бронирований
+    List<Booking> findByItemOwnerIdAndEndBeforeOrderByStartDesc(int ownerId, LocalDateTime end);
 
-    @Query("SELECT booking FROM Booking booking WHERE " +
-            "booking.item.owner.id = ?1 AND booking.start > ?2 " +
-            "ORDER BY booking.start DESC")
-    List<Booking> findFutureByOwnerIdAndStartIsAfter(int ownerId, LocalDateTime start);
+    // Запрос предстоящих бронирований
+    List<Booking> findByItemOwnerIdAndStartAfterOrderByStartDesc(int ownerId, LocalDateTime start);
 
     @Query("SELECT booking FROM Booking booking WHERE " +
             "booking.item.owner.id = ?1 AND (?2 BETWEEN booking.start AND booking.end) " +
