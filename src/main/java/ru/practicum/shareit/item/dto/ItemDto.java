1ccbc3a467eaddb3a224235af7827ebbbd0af063
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-10-17 19:05:49 +0300

    refactor: скорректировать наименования dto для Item

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,5 @@
 package ru.practicum.shareit.item.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.List;
@@ -10,14 +8,11 @@ import java.util.List;
 public class ItemDto {
     private Integer id;
     private Integer ownerId;
-    @NotBlank(message = "Наименование не может быть пустым")
     private String name;
-    @NotBlank(message = "Описание не может быть пустым")
     private String description;
-    @NotNull(message = "Статус должен быть задан")
     private Boolean available;
     private Integer requestId;
-    private String lastBooking = null;
-    private String nextBooking = null;
+    private String lastBooking;
+    private String nextBooking;
     private List<CommentDto> comments;
 }
---
+++ src/main/java/ru/practicum/shareit/item/dto/ItemShortDto.java
@@ -0,0 +1,21 @@
+package ru.practicum.shareit.item.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ItemShortDto {
+    private Integer id;
+    private Integer ownerId;
+    @NotBlank(message = "Наименование не может быть пустым")
+    private String name;
+    @NotBlank(message = "Описание не может быть пустым")
+    private String description;
+    @NotNull(message = "Статус должен быть задан")
+    private Boolean available;
+    private Integer requestId;
+    private List<CommentDto> comments;
+}
