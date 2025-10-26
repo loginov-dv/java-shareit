645a793609aac625fbe4087312bce702dc15bc63
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-10-26 19:28:37 +0300

    fix(BookingDto): привести типы полей в соответствие с новыми наименованиями dto

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,13 +1,13 @@
 package ru.practicum.shareit.booking.dto;
 
 import lombok.Data;
-import ru.practicum.shareit.item.dto.ItemShortDto;
+import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.user.dto.UserDto;
 
 @Data
 public class BookingDto {
     private Integer id;
-    private ItemShortDto item;
+    private ItemDto item;
     private UserDto booker;
     private String start;
     private String end;
