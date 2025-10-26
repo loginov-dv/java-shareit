dfcf3f715809dd444b1f2ce0c888d9a32551d467
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-10-26 19:23:21 +0300

    feat(ItemRequestDto): скорректировать типы полей, добавить новые поля

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,13 +1,15 @@
 package ru.practicum.shareit.request.dto;
 
 import lombok.Data;
+import ru.practicum.shareit.item.dto.ItemShortDto;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 public class ItemRequestDto {
     private Integer id;
     private String description;
     private Integer requestorId;
-    private LocalDateTime created;
+    private String created;
+    private List<ItemShortDto> items;
 }
