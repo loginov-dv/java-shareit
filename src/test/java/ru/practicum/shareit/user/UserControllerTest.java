77e38aa8c7234bde8a1fa543420b5df6bb7ce9ec
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-10-29 22:56:17 +0300

    test(UserControllerTest): добавить использование моков, добавить новые тесты

--- src/test/java/ru/practicum/shareit/user/UserControllerTest.java
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -2,259 +2,240 @@ package ru.practicum.shareit.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
+import ru.practicum.shareit.exception.EmailConflictException;
+import ru.practicum.shareit.exception.ExceptionConstants;
+import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.user.dto.PatchUserRequest;
 import ru.practicum.shareit.user.dto.PostUserRequest;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.utils.RandomUtils;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles("test")
-@SpringBootTest
-@AutoConfigureMockMvc
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Sql(scripts = {"/schema.sql", "/clear.sql"})
+@WebMvcTest(controllers = UserController.class)
 class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
+    @MockBean
+    private UserService userService;
 
     @Test
     void shouldCreateUser() throws Exception {
-        PostUserRequest user = createUser();
+        PostUserRequest newUser = createNewUser();
+        UserDto savedUser = createUserDto(newUser);
+
+        when(userService.createUser(any()))
+                .thenReturn(savedUser);
 
         mockMvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(user)))
+                        .content(objectMapper.writeValueAsString(newUser)))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").exists())
-                .andExpect(jsonPath("$.name").value(user.getName()))
-                .andExpect(jsonPath("$.email").value(user.getEmail()));
+                .andExpect(jsonPath("$.name").value(newUser.getName()))
+                .andExpect(jsonPath("$.email").value(newUser.getEmail()));
     }
 
-    @Test
-    void shouldNotCreateUserWithoutEmail() throws Exception {
-        PostUserRequest user = createUser();
-        user.setEmail(null);
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"", " ", "invalid email", "invalid_email"})
+    void shouldNotCreateUserWithInvalidEmail(String email) throws Exception {
+        PostUserRequest newUser = createNewUser();
+        newUser.setEmail(email);
+
+        UserDto savedUser = createUserDto(newUser);
+
+        when(userService.createUser(any()))
+                .thenReturn(savedUser);
 
         mockMvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(user)))
+                        .content(objectMapper.writeValueAsString(newUser)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void shouldNotCreateUserWithNullOrEmptyName(String name) throws Exception {
+        PostUserRequest newUser = createNewUser();
+        newUser.setName(name);
+
+        UserDto savedUser = createUserDto(newUser);
+
+        when(userService.createUser(any()))
+                .thenReturn(savedUser);
+
+        mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newUser)))
                 .andExpect(status().isBadRequest());
     }
 
     @Test
-    void shouldNotCreateUserWithInvalidEmail() throws Exception {
-        PostUserRequest userWithInvalidEmailFormat = createUser();
-        userWithInvalidEmailFormat.setEmail(userWithInvalidEmailFormat.getName());
+    void shouldNotCreateUserWithAlreadyExistingEmail() throws Exception {
+        when(userService.createUser(any()))
+                .thenThrow(new EmailConflictException(ExceptionConstants.EMAIL_CONFLICT));
 
         mockMvc.perform(post("/users")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(userWithInvalidEmailFormat)))
-                .andExpect(status().isBadRequest());
-
-        PostUserRequest userWithWhitespaceInEmail = createUser();
-        userWithWhitespaceInEmail.setEmail(userWithWhitespaceInEmail.getEmail() + " ");
-
-        mockMvc.perform(post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(userWithInvalidEmailFormat)))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    void shouldNotCreateUserWithoutName() throws Exception {
-        PostUserRequest user = createUser();
-        user.setName(null);
-
-        mockMvc.perform(post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(user)))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    void shouldNotCreateUserWithTheSameEmail() throws Exception {
-        PostUserRequest user1 = createUser();
-        PostUserRequest user2 = createUser();
-        user2.setEmail(user1.getEmail());
-
-        mockMvc.perform(post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(user1)))
-                .andExpect(status().isCreated());
-
-        mockMvc.perform(post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(user2)))
+                        .content(objectMapper.writeValueAsString(createNewUser())))
                 .andExpect(status().isConflict());
     }
 
     @Test
-    void shouldGetUser() throws Exception {
-        PostUserRequest user = createUser();
+    void shouldGetUserById() throws Exception {
+        UserDto savedUser = createUserDto(createNewUser());
 
-        MvcResult response = mockMvc.perform(post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(user)))
-                .andExpect(status().isCreated())
-                .andReturn();
+        when(userService.findById(savedUser.getId()))
+                .thenReturn(savedUser);
 
-        String json = response.getResponse().getContentAsString();
-        UserDto userDto = objectMapper.readValue(json, UserDto.class);
-
-        mockMvc.perform(get("/users/" + userDto.getId())
-                .contentType(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/users/" + savedUser.getId())
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(userDto.getId()))
-                .andExpect(jsonPath("$.name").value(user.getName()))
-                .andExpect(jsonPath("$.email").value(user.getEmail()));
+                .andExpect(jsonPath("$.id").value(savedUser.getId()))
+                .andExpect(jsonPath("$.name").value(savedUser.getName()))
+                .andExpect(jsonPath("$.email").value(savedUser.getEmail()));
     }
 
     @Test
-    void shouldDeleteUser() throws Exception {
-        PostUserRequest user = createUser();
+    void shouldNotGetUnknownUserById() throws Exception {
+        when(userService.findById(999))
+                .thenThrow(new NotFoundException(String.format(ExceptionConstants.USER_NOT_FOUND_BY_ID, 999)));
 
-        MvcResult response = mockMvc.perform(post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(user)))
-                .andExpect(status().isCreated())
-                .andReturn();
-
-        String json = response.getResponse().getContentAsString();
-        UserDto userDto = objectMapper.readValue(json, UserDto.class);
-
-        mockMvc.perform(delete("/users/" + userDto.getId())
+        mockMvc.perform(get("/users/" + 999)
                         .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNoContent());
-
-        mockMvc.perform(get("/users/" + userDto.getId()))
                 .andExpect(status().isNotFound());
     }
 
     @Test
-    void shouldUpdateUser() throws Exception {
-        PostUserRequest user = createUser();
+    void shouldDeleteUserById() throws Exception {
+        doNothing().when(userService).deleteById(anyInt());
 
-        MvcResult response = mockMvc.perform(post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(user)))
-                .andExpect(status().isCreated())
-                .andReturn();
-
-        String json = response.getResponse().getContentAsString();
-        UserDto userDto = objectMapper.readValue(json, UserDto.class);
-
-        PatchUserRequest updatedUser = new PatchUserRequest();
-        updatedUser.setName("new name");
-        updatedUser.setEmail("new@mail.ru");
-
-        mockMvc.perform(patch("/users/" + userDto.getId())
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(updatedUser)))
-                .andExpect(status().isOk());
-
-        mockMvc.perform(get("/users/" + userDto.getId())
+        mockMvc.perform(delete("/users/" + 999)
                         .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+        verify(userService, Mockito.times(1)).deleteById(999);
+    }
+
+    @Test
+    void shouldUpdateUser() throws Exception {
+        PatchUserRequest request = new PatchUserRequest();
+        UserDto updatedUser = new UserDto();
+        request.setName("name");
+        request.setEmail("name@mail.ru");
+        updatedUser.setId(1);
+        updatedUser.setName(request.getName());
+        updatedUser.setEmail(request.getEmail());
+
+        when(userService.update(anyInt(), any()))
+                .thenReturn(updatedUser);
+
+        mockMvc.perform(patch("/users/" + 1)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updatedUser)))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(userDto.getId()))
+                .andExpect(jsonPath("$.id").value(updatedUser.getId()))
                 .andExpect(jsonPath("$.name").value(updatedUser.getName()))
                 .andExpect(jsonPath("$.email").value(updatedUser.getEmail()));
     }
 
-    @Test
-    void shouldNotUpdateUserWithInvalidName() throws Exception {
-        PostUserRequest user = createUser();
+    @ParameterizedTest
+    @ValueSource(strings = {" ", ""})
+    void shouldNotUpdateUserIfNewNameIsInvalid(String name) throws Exception {
+        PatchUserRequest request = new PatchUserRequest();
+        request.setName(name);
+        request.setEmail("name@mail.ru");
 
-        MvcResult response = mockMvc.perform(post("/users")
+        mockMvc.perform(patch("/users/" + 1)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(user)))
-                .andExpect(status().isCreated())
-                .andReturn();
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+    }
 
-        String json = response.getResponse().getContentAsString();
-        UserDto userDto = objectMapper.readValue(json, UserDto.class);
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "invalid email", "invalid_email"})
+    void shouldNotUpdateUserIfNewEmailIsInvalid(String email) throws Exception {
+        PatchUserRequest request = new PatchUserRequest();
+        request.setName("name");
+        request.setEmail(email);
 
-        PatchUserRequest updatedUser = new PatchUserRequest();
-        updatedUser.setName("");
-
-        mockMvc.perform(patch("/users/" + userDto.getId())
+        mockMvc.perform(patch("/users/" + 1)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(updatedUser)))
+                        .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isBadRequest());
     }
 
     @Test
-    void shouldNotUpdateUserWithInvalidEmail() throws Exception {
-        PostUserRequest user = createUser();
+    void shouldNotUpdateUnknownUser() throws Exception {
+        when(userService.update(anyInt(), any()))
+                .thenThrow(new NotFoundException(String.format(ExceptionConstants.USER_NOT_FOUND_BY_ID, 999)));
 
-        MvcResult response = mockMvc.perform(post("/users")
+        PatchUserRequest request = new PatchUserRequest();
+        request.setName("name");
+        request.setEmail("name@mail.ru");
+
+        mockMvc.perform(patch("/users/" + 999)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(user)))
-                .andExpect(status().isCreated())
-                .andReturn();
-
-        String json = response.getResponse().getContentAsString();
-        UserDto userDto = objectMapper.readValue(json, UserDto.class);
-
-        PatchUserRequest updatedUser = new PatchUserRequest();
-        updatedUser.setEmail("");
-
-        mockMvc.perform(patch("/users/" + userDto.getId())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(updatedUser)))
-                .andExpect(status().isBadRequest());
-
-        updatedUser = new PatchUserRequest();
-        updatedUser.setEmail("email");
-
-        mockMvc.perform(patch("/users/" + userDto.getId())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(updatedUser)))
-                .andExpect(status().isBadRequest());
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isNotFound());
     }
 
     @Test
-    void shouldNotUpdateUserWithTheSameEmail() throws Exception {
-        PostUserRequest user1 = createUser();
-        PostUserRequest user2 = createUser();
+    void shouldNotUpdateUserIfNewEmailAlreadyExists() throws Exception {
+        when(userService.update(anyInt(), any()))
+                .thenThrow(new EmailConflictException(ExceptionConstants.EMAIL_CONFLICT));
 
-        mockMvc.perform(post("/users")
+        PatchUserRequest request = new PatchUserRequest();
+        request.setName("name");
+        request.setEmail("name@mail.ru");
+
+        mockMvc.perform(patch("/users/" + 999)
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(user1)))
-                .andExpect(status().isCreated());
-
-        MvcResult response = mockMvc.perform(post("/users")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(user2)))
-                .andExpect(status().isCreated())
-                .andReturn();
-
-        String json = response.getResponse().getContentAsString();
-        UserDto userDto2 = objectMapper.readValue(json, UserDto.class);
-
-        PatchUserRequest updatedUser2 = new PatchUserRequest();
-        updatedUser2.setEmail(user1.getEmail());
-
-        mockMvc.perform(patch("/users/" + userDto2.getId())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(updatedUser2)))
+                        .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isConflict());
     }
 
-    private PostUserRequest createUser() {
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0})
+    void shouldReturnBadRequestIfUserIdNotPositive(int id) throws Exception {
+        mockMvc.perform(get("/users/" + id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        mockMvc.perform(delete("/users/" + id)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        PatchUserRequest request = new PatchUserRequest();
+        request.setName("name");
+        request.setEmail("name@mail.ru");
+
+        mockMvc.perform(patch("/users/" + id)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+    }
+
+    private PostUserRequest createNewUser() {
         PostUserRequest user = new PostUserRequest();
         String name = RandomUtils.createName();
 
@@ -262,5 +243,15 @@ class UserControllerTest {
         user.setEmail(name + "@mail.ru");
 
         return user;
+    }
+
+    private UserDto createUserDto(PostUserRequest request) {
+        UserDto dto = new UserDto();
+
+        dto.setId(1);
+        dto.setName(request.getName());
+        dto.setEmail(request.getEmail());
+
+        return dto;
     }
 }
