01628b2188b5deda46012f779f47d3bfcc4563d2
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-10-31 21:33:42 +0300

    refactor(ItemServiceImplTest): удалить неиспользуемые импорты

--- src/test/java/ru/practicum/shareit/item/ItemServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemServiceImplTest.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.item;
 
-import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
