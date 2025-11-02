d0c992100cfd23131d7252e0ef270d3ef3401c35
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-11-02 16:30:34 +0300

    feat: перенести класс с методом main в модуль server

--- shareIt-server/src/main/java/ru/practicum/shareit/server/ShareItServer.java
+++ shareIt-server/src/main/java/ru/practicum/shareit/server/ShareItServer.java
@@ -1,13 +1,11 @@
-package ru.practicum.shareit;
+package ru.practicum.shareit.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShareItApp {
-
+public class ShareItServer {
 	public static void main(String[] args) {
-		SpringApplication.run(ShareItApp.class, args);
+		SpringApplication.run(ShareItServer.class, args);
 	}
-
 }
