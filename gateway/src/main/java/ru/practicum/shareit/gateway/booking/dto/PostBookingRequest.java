8fc3bff5f3b48d2c138bf8d3fbc2f134aca0907b
DmitryLoginov <v94-gb4h4@yandex.ru>
2025-11-03 12:02:58 +0300

    fix(PostBookingRequest): скорректировать тип полей start и end, добавить аннотации @FutureOrPresent и @Future

--- gateway/src/main/java/ru/practicum/shareit/gateway/booking/dto/PostBookingRequest.java
+++ gateway/src/main/java/ru/practicum/shareit/gateway/booking/dto/PostBookingRequest.java
@@ -1,14 +1,20 @@
 package ru.practicum.shareit.gateway.booking.dto;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 public class PostBookingRequest {
     @NotNull(message = "Id бронируемого предмета не может быть пустым")
     private Integer itemId;
     @NotNull(message = "Дата начала бронирования не может быть пустой")
-    private String start;
+    @FutureOrPresent(message = "Дата начала бронирования не может быть в прошлом")
+    private LocalDateTime start;
     @NotNull(message = "Дата окончания бронирования не может быть пустой")
-    private String end;
+    @Future(message = "Дата окончания бронирования не может быть в прошлом")
+    private LocalDateTime end;
 }
